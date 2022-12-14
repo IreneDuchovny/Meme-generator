519c8749abd34cb147eccff1cea70a2b162ba15f
Irene Duchovny <ibalakin@gmail.com>
2022-12-14 11:34:30 +0200

    canvas update initial

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -1,1 +1,35 @@
 'use strict'
+let gElCanvas
+let gCtx
+
+
+//creates a new canvas
+function onMemeInit() {
+    gElCanvas = document.querySelector('#canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    //TODO: resizeCanvas()
+    // window.addEventListener('resize', () => {
+    // resizeCanvas()
+
+    renderMeme()
+}
+
+//renders currState of canvas
+function renderMeme() {
+    var img = new Image()
+
+    img.src = gMeme.selectedImgUrl
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        gMeme.lines.forEach(line => {
+            gCtx.lineWidth = '2';
+            // gCtx.strokeStyle = line.strokeColor;
+            gCtx.fillStyle = line.color;
+            gCtx.font = `${line.size}px ${line.font}`;
+            gCtx.textAlign = line.align;
+            gCtx.fillText(line.txt, line.x, line.y);
+            gCtx.strokeText(line.txt, line.x, line.y);
+        })
+    }
+}
---
+++ js/services/meme-service.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedImgUrl: 'img/5.jpg',
+    selectedLineIdx: 0,
+
+    lines: [
+        {
+            txt: 'I am a meme',
+            size: 40,
+            align: 'center',
+            color: 'white',
+            font: 'Impact',
+            x: 250,
+            y: 50
+            //TODO: CENTER X,Y
+        }
+    ]
+}
